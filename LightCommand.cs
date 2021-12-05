6313b6b9d1e47cb38ce3a279630052bab9bd30c4
davidTigris <g.musset@mailbox.org>
2021-12-05 16:44:06 +0100

    Initial commit

---
+++ LightCommand.cs
@@ -0,0 +1,22 @@
+﻿namespace CommandPattern
+{
+    public class LightCommand : ICommand
+    {
+        private readonly Lamp _lamp;
+
+        public LightCommand(Lamp lamp)
+        {
+            _lamp = lamp;
+        }
+
+        public void Execute()
+        {
+            _lamp.TurnOn();
+        }
+
+        public void Revert()
+        {
+            _lamp.TurnOff();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CommandPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lamp lamp = new();
+            LightCommand lightCommand = new(lamp);
+
+            RemoteControl remote = new(lightCommand);
+            remote.ExecuteCommand();
+            remote.UndoCommand();
+        }
+    }
+}
